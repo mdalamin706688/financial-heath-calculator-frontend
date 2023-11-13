c7c39123ccc1fa293f057c52c2c1c21becc20fc6
Md Alamin HAque <73795242+mdalamin706688@users.noreply.github.com>
2023-11-14 00:19:31 +0600

    Update api.js

--- src/components/Services/api.js
+++ src/components/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001', // Your API base URL
+  baseURL: 'https://financial-health-calculator-l7eu.onrender.com', // Your API base URL
 });
 
 export default instance;
